c1bff3e16265aba57e32239672629f1bf4a26aee
AbdullahAlhussein <abdullah30alhussein@gmail.com>
2021-12-12 21:10:41 +0300

    create package category

--- demo/src/main/java/com/example/demo/category/CategoryService.java
+++ demo/src/main/java/com/example/demo/category/CategoryService.java
@@ -1,4 +1,42 @@
 package com.example.demo.category;
 
+import com.example.demo.user.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class CategoryService {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    public Category createUser(Category category) {
+        return categoryRepository.save(category);
+    }
+
+    public List<Category> getAllCategory() {
+        return categoryRepository.findAll();
+    }
+
+    public Category getCategory(String id) {
+        Long categoryId =Long.parseLong(id);
+        return categoryRepository.findById(categoryId).orElse(null);
+    }
+
+    public void updateCategory(String id, Category data) {
+        Long updateCategory = Long.parseLong(id);
+        Category category = categoryRepository.findById(updateCategory).orElse(null);
+
+        if(category != null){
+            category.setCategoryName(data.getCategoryName());
+            categoryRepository.save(category);
+        }
+    }
+
+    public void deleteCategory(String id) {
+        Long deleteId = Long.parseLong(id);
+        categoryRepository.deleteById(deleteId);
+    }
 }
